1e493b7f96f7bec88041c38a037cf39eb46876b7
Rohit <rohitkul247@gmail.com>
2021-08-21 13:52:25 +0530

    successful basic routing

---
+++ server/app.js
@@ -0,0 +1,37 @@
+//jshint version:6
+
+// require('dotenv').config();
+import express from "express";
+import bodyparser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+import UserRoute from "./routes/helper.js";
+
+const app = express();
+app.use(cors());
+
+// app.use(express.static("public"));
+// app.set("view engine", "ejs");
+
+app.use(express.json());
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: true }));
+
+app.use("/auth", UserRoute);
+
+mongoose.connect("mongodb://localhost:27017/interviewcrossover", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+mongoose.set("useCreateIndex", true);
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 5000;
+}
+
+app.listen(port, function () {
+  console.log("server running on port 5000");
+});
