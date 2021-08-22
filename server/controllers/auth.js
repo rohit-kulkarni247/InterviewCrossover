059cabba8b4e038195ebb4520d1a03121aa2bb92
Rohit <rohitkul247@gmail.com>
2021-08-22 10:05:10 +0530

    added login router

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -18,3 +18,23 @@ export const signup = async (req, res) => {
     res.sendStatus(500);
   }
 };
+
+export const login = async (req, res) => {
+  const user = await InterviewCreator.findOne({
+    email: req.body.email,
+  });
+  if (user) {
+    if (user.password === req.body.password) {
+      res.status(200).json(user);
+    } else {
+      res.sendStatus(401);
+    }
+  } else {
+    res.sendStatus(404);
+  }
+};
+
+export const getUsers = async (req, res) => {
+  const users = await InterviewCreator.find({}).exec();
+  res.status(200).json(users);
+};
--- server/routes/helper.js
+++ server/routes/helper.js
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { signup,login } from "../controllers/auth.js";
+import { signup, login, getUsers } from "../controllers/auth.js";
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
+router.get("/getallusers", getUsers);
 
 export default router;
