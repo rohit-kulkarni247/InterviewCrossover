38125ed1bc9e5ba9030d3649a80b4052efe1f27f
Rohit <rohitkul247@gmail.com>
2021-08-30 18:24:31 +0530

    added jwt and hashing to serverside

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,19 +1,39 @@
 import express from "express";
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
 
 import InterviewCreator from "../models/user.js";
 
 const router = express.Router();
 
 export const signup = async (req, res) => {
+  const { fullname, email, password } = req.body;
+  const candidate = await InterviewCreator.findOne({ email });
+  if (candidate) {
+    return res.status(400).json({
+      error: "User already exists",
+    });
+  }
+  const hashedPassword = await bcrypt.hash(password, 12);
   var reactuser = new InterviewCreator({
-    fullname: req.body.fullname,
-    email: req.body.email,
-    password: req.body.password,
+    fullname: fullname,
+    email: email,
+    password: hashedPassword,
   });
   try {
     await reactuser.save();
-    res.status(201).json(reactuser);
+    const token = jwt.sign(
+      {
+        email: reactuser.email,
+        userId: reactuser._id,
+      },
+      "asdkjfqlwqn123894asdkjlfkb21983hncanskdj1i324rasdbjkf89yt5wh7g",
+      {
+        expiresIn: "1h",
+      }
+    );
+    res.status(201).json({ reactuser, token });
   } catch (err) {
     res.sendStatus(500);
   }
@@ -24,8 +44,14 @@ export const login = async (req, res) => {
     email: req.body.email,
   });
   if (user) {
-    if (user.password === req.body.password) {
-      res.status(200).json(user);
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+    if (isMatch) {
+      const token = jwt.sign(
+        { email: user.email, id: user._id },
+        "asdkjfqlwqn123894asdkjlfkb21983hncanskdj1i324rasdbjkf89yt5wh7g",
+        { expiresIn: "1h" }
+      );
+      res.status(200).json({ user, token });
     } else {
       res.sendStatus(401);
     }
