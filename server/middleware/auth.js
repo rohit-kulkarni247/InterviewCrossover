dead1dbb78044def1f2a4600ef6ba7d86e2659e5
Rohit <rohitkul247@gmail.com>
2021-08-30 18:31:22 +0530

    Added middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(
+      token,
+      "asdkjfqlwqn123894asdkjlfkb21983hncanskdj1i324rasdbjkf89yt5wh7g"
+    );
+    req.user = decoded;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: "Not authorized to access this route" });
+  }
+};
+
+export default auth;
