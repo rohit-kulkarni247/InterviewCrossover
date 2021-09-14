3ebfa8be76d88e4ed15d31a13ec1c98966152560
Rohit <rohitkul247@gmail.com>
2021-09-14 11:09:53 +0530

    added image and text

--- client/src/components/Landing.jsx
+++ client/src/components/Landing.jsx
@@ -2,13 +2,39 @@ import React from "react";
 import Navbar from "./UI/Navbar";
 import "./Landing.css";
 import laptop from "../laptop.png";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+}));
 function Landing() {
+  const classes = useStyles();
+
   return (
     <div>
       <Navbar />
-      <img src={laptop} alt="laptop" className="laptop" />
+      <div className={classes.root}>
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <img src={laptop} alt="laptop" className="laptop" />
+          </Grid>
+          <Grid item xs={6}>
+            <h2>hello world</h2>
+          </Grid>
+        </Grid>
+      </div>
     </div>
+    /* <img src={laptop} alt="laptop" className="laptop" />
+      <h2>hello world</h2>*/
   );
 }
 
