7a7f0bfb970d8a4f39a474e376f6886b812305a7
Rohit <rohitkul247@gmail.com>
2021-09-28 10:50:17 +0530

    added dropdown for company selection

--- client/src/components/GetPosts.jsx
+++ client/src/components/GetPosts.jsx
@@ -1,9 +1,19 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
 import { Link } from "react-router-dom";
 
 function GetPosts() {
   const [data, dataHandler] = useState([]);
+  const [age, setAge] = React.useState("");
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+    console.log(age);
+  };
 
   useEffect(() => {
     getPosts();
@@ -13,7 +23,7 @@ function GetPosts() {
     axios
       .get("http://localhost:5000/auth/getallposts")
       .then(function (response) {
-        console.log(response);
+        // console.log(response);
         dataHandler(response.data);
       })
       .catch(function (error) {
@@ -22,6 +32,30 @@ function GetPosts() {
   };
   return (
     <div>
+      <FormControl variant="standard" style={{ display: "flex", width: "30%" }}>
+        <InputLabel
+          id="demo-simple-select-label"
+          style={{ marginLeft: "auto" }}
+        >
+          Select Company
+        </InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={age}
+          label="Age"
+          onChange={handleChange}
+        >
+          {data.map((item) => {
+            return (
+              <MenuItem value={item.companyName}>{item.companyName}</MenuItem>
+            );
+          })}
+          {/* <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem> */}
+        </Select>
+      </FormControl>
       {data.map(function (post) {
         return (
           <div key={post._id}>
